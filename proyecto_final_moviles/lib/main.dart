2ec0ec8795536cf16137ffb036c9275077b490c7
KenetGaona115 <aldrinkenet@hotmail.com>
2020-02-25 18:48:34 -0600

    Listo al 25/02/2020

---
+++ proyecto_final_moviles/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_final_moviles/sscreen.dart';
+ 
+void main() => runApp(SplashScreen());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: SplashScreen(),
+      ),
+    );
+  }
+}
+
---
+++ proyecto_final_moviles/lib/sscreen.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:proyecto_final_moviles/Login/login.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State createState() {
+    // TODO: implement createState
+    return _SplashScreenState();
+  }
+}
+
+class _SplashScreenState extends State {
+  @override
+  void initState() {
+    // TODO: implement initState
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Colors.blueGrey),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 70.0,
+                        child: CircleAvatar(
+                          radius: 60.0,
+                          backgroundImage: NetworkImage(
+                              "https://pngimage.net/wp-content/uploads/2018/06/uber-icon-png-1.png"),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "Splash Screen",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator(),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Text(
+                      "La mejor opcion para tus pedidos",
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    ));
+  }
+}
+
+
